9efb4131ba4c9132acc1cbcdf4bdc58812ccb252
zelo <zelo@zelo.pl>
2020-05-24 10:13:01 +0200

    Feed data trough pipes instead using temporary files during audio conversion

--- stt/engine.py
+++ stt/engine.py
@@ -1,29 +1,28 @@
-from os import getenv
-from pathlib import Path
-from subprocess import run
-from tempfile import NamedTemporaryFile
 import wave
+from io import BytesIO
+from pathlib import Path
 
-from deepspeech import Model
+import ffmpeg
 import numpy as np
-
-
-MODEL_PATH = Path(getenv('MODEL_PATH', Path(__file__).parents[1].joinpath('model.pbmm').absolute()))
-FFMPEG_PATH = getenv('FFMPEG_PATH', 'ffmpeg')
+from deepspeech import Model
 
 
 class SpeechToTextEngine:
     def __init__(self):
-        self.model = Model(model_path=MODEL_PATH.as_posix())
+        self.model = Model(model_path=Path(__file__).parents[1].joinpath('model.pbmm').absolute().as_posix())
+
+    def normalize_audio(self, audio):
+        out, err = ffmpeg.input('pipe:0') \
+            .output('pipe:1', f='WAV', acodec='pcm_s16le', ac=1, ar='16k', loglevel='error', hide_banner=None) \
+            .run(input=audio, capture_stdout=True, capture_stderr=True)
+        if err:
+            raise Exception(err)
+        return out
 
     def run(self, audio):
-        with NamedTemporaryFile() as in_file, NamedTemporaryFile() as out_file:
-            in_file.write(audio)
-            # TODO: consider piping data, but in some cases ffmpeg whines about missing headers
-            cmd = (f"{FFMPEG_PATH} -loglevel error -hide_banner -i {in_file.name}"
-                   f" -y -f WAV -ar 16000 -ac 1 {out_file.name}")
-            run(cmd.split(), check=True)
-            with wave.open(out_file.name, 'rb') as wav:
-                audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
-            result = self.model.stt(audio_buffer=audio)
-            return result
+        audio = self.normalize_audio(audio)
+        audio = BytesIO(audio)
+        with wave.Wave_read(audio) as wav:
+            audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
+        result = self.model.stt(audio_buffer=audio)
+        return result
